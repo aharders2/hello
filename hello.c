adebab50bb2cae68b51e197e6e5a5baabed42c4a
aharders2 <54557520+aharders2@users.noreply.github.com>
2019-08-29 19:38:57 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -9,12 +9,10 @@
 int main()
 {
  printf("Andrew\n");
-<<<<<<< HEAD
  printf("computer science\n");
-=======
- printf("computer science\n");
+
  printf("Bryce\n");
  printf("Computer science\n");
->>>>>>> d1106cc43d739c94edd79a21accdafb6ff73860d
+
  return 0;
 }
